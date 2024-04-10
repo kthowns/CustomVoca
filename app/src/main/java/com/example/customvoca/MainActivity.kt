0a203c4e99d23a44959b88107fd2fd7f3ff9d075
KTH <pni2396@gmail.com>
2024-04-10 14:52:40 +0900

    0.00 Primary State [개발 시작]

--- app/src/main/java/com/example/customvoca/MainActivity.kt
+++ app/src/main/java/com/example/customvoca/MainActivity.kt
@@ -8,7 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
     }
 }
