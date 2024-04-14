a0461afdb571753b8adbd0e00cc26872df2e70b2
KTH <pni2396@gmail.com>
2024-04-14 16:26:29 +0900

    fix: ViewModel 문법 변경
    ViewModel 불러오는 코드를 ViewModelProvider 대신 viewModels로 변경하고 Activity에 private로 선언

--- app/src/main/java/com/example/customvoca/MainActivity.kt
+++ app/src/main/java/com/example/customvoca/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.customvoca
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.customvoca.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
