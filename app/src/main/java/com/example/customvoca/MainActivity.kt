ed0c1e204e3f330509bdc139988807a5a43014a0
KTH <pni2396@gmail.com>
2024-05-23 13:46:33 +0900

    s

--- app/src/main/java/com/example/customvoca/MainActivity.kt
+++ app/src/main/java/com/example/customvoca/MainActivity.kt
@@ -18,6 +18,5 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil
             .setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
-
     }
 }
