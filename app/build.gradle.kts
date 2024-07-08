e319457217beea49d7af4ef6156b9277e1a936ec
KTH <pni2396@gmail.com>
2024-07-08 22:54:49 +0900

    feat: BottomNavigationView 추가 아직 진행중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,10 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
