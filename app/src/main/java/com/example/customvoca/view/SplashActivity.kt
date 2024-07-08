93ad9111e3c193c3961a65b8dc0f6c834fdcd779
KTH <pni2396@gmail.com>
2024-07-08 22:19:25 +0900

    feat: Splash Theme 추가 간단한 Splash Theme 추가함

--- app/src/main/java/com/example/customvoca/view/SplashActivity.kt
+++ app/src/main/java/com/example/customvoca/view/SplashActivity.kt
@@ -3,14 +3,20 @@ package com.example.customvoca.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.customvoca.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        startActivity(Intent(this, SplashActivity::class.java))
-        finish()
+        lifecycleScope.launch{
+            delay(1500)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
